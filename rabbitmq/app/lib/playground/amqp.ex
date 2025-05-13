29abb642b5546afaab3fd64d69968f5ad2b8e082
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2025-05-14 00:12:42 +0200

    fix: lowercase record name

--- rabbitmq/app/lib/playground/amqp.ex
+++ rabbitmq/app/lib/playground/amqp.ex
@@ -2,9 +2,12 @@ defmodule Playground.AMQP do
   require Record
 
   for {tag, fields} <- Record.extract_all(from_lib: "amqp_client/include/amqp_client.hrl") do
-    Atom.to_string(tag)
-    |> String.replace(".", "_")
-    |> String.to_atom()
-    |> Record.defrecord(tag, fields)
+    name =
+      Atom.to_string(tag)
+      |> String.replace(".", "_")
+      |> String.downcase()
+      |> String.to_atom()
+
+    Record.defrecord(name, tag, fields)
   end
 end
